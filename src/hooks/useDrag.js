76cc2095362ccec69f10ecb4dd9bdead230c61e6
javaLuo <376693576@qq.com>
2023-09-02 21:40:53 +0800

    update

--- src/hooks/useDrag.js
+++ src/hooks/useDrag.js
@@ -1,69 +1,79 @@
-import {ref, onMounted, watch} from 'vue';
-import anime from 'animejs';
+import { ref, watch } from "vue";
+import anime from "animejs";
 
-export default function useDrag(dom){
-    const d = ref(null);
-    const touchStartInfo = ref({x: 0, y: 0});
-    const touchPrevInfo =  ref({x: 0, y: 0});
-    const transPrev = ref({x: 0, y: 0});
+export default function useDrag(dom) {
+  const d = ref(null);
+  const touchStartInfo = ref({ x: 0, y: 0 });
+  const touchPrevInfo = ref({ x: 0, y: 0 });
+  const transPrev = ref({ x: 0, y: 0 });
+  const isDraging = ref(false);
+  const isActive = ref(true); // 是否可以拖拽
 
-    watch(()=> dom.value, (newV)=>{
-        d.value = newV;
+  watch(
+    () => dom.value,
+    newV => {
+      d.value = newV;
+    },
+  );
+
+  function onTouchStart(e) {
+    if (!isActive.value) return;
+    isDraging.value = true;
+    touchStartInfo.value = {
+      x: e.touches[0].clientX,
+      y: e.touches[0].clientY,
+    };
+  }
+
+  function onTouchMove(e) {
+    if (!isDraging.value) return;
+    const touchNow = e.touches[0];
+    const diffX = transPrev.value.x + touchNow.clientX - touchStartInfo.value.x;
+    const diffY = transPrev.value.y + touchNow.clientY - touchStartInfo.value.y;
+    touchPrevInfo.value.x = diffX;
+    touchPrevInfo.value.y = diffY;
+
+    d.value.style.translate = `${diffX}px ${diffY}px`;
+  }
+
+  function onTouchEnd() {
+    if (!isDraging.value) return;
+    transPrev.value = {
+      x: touchPrevInfo.value.x,
+      y: touchPrevInfo.value.y,
+    };
+
+    const battery = {
+      x: touchPrevInfo.value.x,
+      y: touchPrevInfo.value.y,
+    };
+
+    touchPrevInfo.value = { x: 0, y: 0 };
+    transPrev.value = { x: 0, y: 0 };
+    isDraging.value = false;
+    anime({
+      targets: battery,
+      x: 0,
+      y: 0,
+      round: 1,
+      easing: "linear",
+      duration: 200,
+      update: function () {
+        d.value.style.translate = `${battery.x}px ${battery.y}px`;
+      },
     });
+  }
 
-    onMounted(()=>{
-        console.log('onMounted');
-    });
+  function setActive(type) {
+    isActive.value = type;
+  }
 
-    function onTouchStart(e){
-        // console.log('onTouchStart', e);
-        touchStartInfo.value = {
-            x: e.touches[0].clientX,
-            y: e.touches[0].clientY,
-        }
-    }
-
-    function onTouchMove(e){
-        // console.log('onTouchMove', d.value.style.translate);
-        const touchNow = e.touches[0];
-        const diffX = transPrev.value.x + touchNow.clientX - touchStartInfo.value.x;
-        const diffY = transPrev.value.y + touchNow.clientY - touchStartInfo.value.y;
-        touchPrevInfo.value.x = diffX;
-        touchPrevInfo.value.y = diffY;
-
-        d.value.style.translate = `${diffX}px ${diffY}px`;
-    }
-
-    function onTouchEnd(){
-        transPrev.value = {
-            x: touchPrevInfo.value.x,
-            y: touchPrevInfo.value.y
-        }
-
-        const battery = {
-            x: touchPrevInfo.value.x,
-            y: touchPrevInfo.value.y
-        }
-
-        touchPrevInfo.value = {x: 0, y: 0};
-        transPrev.value = {x: 0, y: 0};
-        anime({
-            targets: battery,
-            x: 0,
-            y: 0,
-            round: 1,
-            easing: 'linear',
-            duration: 200,
-            update: function() {
-            //   logEl.innerHTML = JSON.stringify(battery);
-              d.value.style.translate = `${battery.x}px ${battery.y}px`;
-            },
-          });
-    }
-
-    return {
-        onTouchStart,
-        onTouchMove,
-        onTouchEnd
-    }
+  return {
+    isDraging,
+    isActive,
+    onTouchStart,
+    onTouchMove,
+    onTouchEnd,
+    setActive,
+  };
 }
